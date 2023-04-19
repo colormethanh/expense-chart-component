d11c983f9243eaa894a9ba42a6fca8b2cbcbe915
colormethanh <thnguyen1847@gmail.com>
2023-04-19 18:54:11 +0900

    did some work

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
 import { useState } from 'react'
 import './App.css'
+import Footer from './Footer'
+import{ ReactComponent as Logo } from './assets/images/logo.svg'
+
+
+function Bar() {
+
+}
 
 function App() {
 
   return (
     <div className="App">
-      <div>
-        My balance
-        $921.48
-
-        Spending - Last 7 days
-
-        mon
-        tue
-        wed
-        thu
-        fri
-        sat
-        sun
-
-        Total this month
-        $478.33
-
-        +2.4%
-        from last month
+      <div className="wrapper">
         
-        <div class="attribution">
-          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-          Coded by <a href="#">Your Name Here</a>.
+        <div className="balance-cont">
+          <div className="balance-txt">
+            <span className="title-sm">My balance</span>
+            <span className="title-lg">$921.48</span>
+          </div>
+          <div className="logo">
+            <Logo />
+          </div>
         </div>
+
+        <div className="chart-cont">
+          <div className="title-lg">
+            Spending - Last 7 days
+          </div>
+          
+          <div className="chart-bars-cont">
+            
+            <Bar />
+            {/* mon
+            tue
+            wed
+            thu
+            fri
+            sat
+            sun */}
+          </div>
+          
+          <hr></hr>
+
+          <div className="summary">
+            <div className="summary-total">
+              <span className='title-sm' style={{color:"hsl(28, 10%, 53%)"}}>Total this month</span>
+              <span className='title-lg'>$478.33</span>
+            </div>
+
+            <div className="summary-percentage title-sm">
+              <span style={{fontWeight: 700, color:"hsl(25, 47%, 15%)"}}>+2.4%</span>
+              <span style={{color:"hsl(28, 10%, 53%)"}}>from last month</span>
+            </div>
+            
+          </div>
+          
+        </div>
+
       </div>
+      <Footer />
     </div>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+
+export default function Footer() {
+  return (
+    <div className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+          Coded by <a href="https://github.com/colormethanh/expense-chart-component">Thanh</a>.
+    </div>
+  )
+}
